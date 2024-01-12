cce3926e76324bbfbfc4ea114937478c50d63a0d
meatwad787 <greg_mrjohnson@yahoo.com>
2024-01-12 17:13:33 -0500

    Add computer's randomly generated number function
    Added the function for the computer to generate a number
    between 1 and 3.
    
    Used 'if' and 'else if' statements to associate each
    number with a specific string. 'rock' 'paper' or 'scissors'
    
    Made sure to add comments for every step of the function.

--- rps.js
+++ rps.js
@@ -1,5 +1,20 @@
 //Make the computer choose between
 //Rock Paper or Scissors
-function getComputerChoice() {
+function getRandomChoice() {
+//Declare a variable to generate & hold the random number
+    let randomNum = Math.floor(Math.random() * 3) + 1;
     
-}
+//Use a conditional to return a string when a number is generated 
+//This ties the number 1 to the string "Rock"
+    if (randomNum === 1) {
+        return 'Rock'
+    }
+//This ties the number 2 to the string "Paper"
+    else if (randomNum === 2) {
+        return 'Paper'
+    }
+//This ties the number 3 to the string "Scissors"
+    else if (randomNum === 3) {
+        return 'Scissors'
+    }
+} console.log(getRandomChoice());
